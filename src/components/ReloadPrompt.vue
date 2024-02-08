717e9cf65ae80f40f721a2e8dee586e61cb8c5f4
Michael Toohig <michael.toohig@gmail.com>
2024-02-09 09:02:26 +1100

    Remove offline ready notice

--- src/components/ReloadPrompt.vue
+++ src/components/ReloadPrompt.vue
@@ -42,17 +42,17 @@ async function close() {
 
 <template>
   <div class="fixed z-[100] bottom-16 left-0 p-4 w-full">
-    <div v-if="offlineReady || needRefresh" class="pwa-toast alert" role="alert">
-      <HandThumbUpIcon v-if="offlineReady" class="shrink-0 w-6 h-6" />
+    <div v-if="needRefresh" class="pwa-toast alert" role="alert">
+      <HandThumbUpIcon v-if="offlineReady && false" class="shrink-0 w-6 h-6" />
       <InformationCircleIcon v-else class="shrink-0 w-6 h-6" />
-      <div v-if="offlineReady">
+      <div v-if="offlineReady && false">
         <h3 class="font-bold">Offline Ready</h3>
         <span class="font-normal">This app can work while offline so you don't have to worry about network to do you work.</span>
       </div>
       <span v-if="offlineReady"></span>
       <span v-else> New content available, click <i>reload</i> to update. </span>
       <div>
-        <button class="btn btn-sm" @click="close">Okay</button>
+        <button class="btn btn-sm" @click="close">Later</button>
         <button class="btn btn-sm btn-primary" v-if="needRefresh" @click="updateServiceWorker()">Reload</button>
       </div>
     </div>
